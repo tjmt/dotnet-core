59c5ba5c5db2c526b3dbfa09e91b5aa0546eeacc
ElvisFabian <elvis.fabian@sigma.com.br>
2019-10-31 17:40:50 -0400

    aspnet 3.0

--- aspnet/3.0/build.sh
+++ aspnet/3.0/build.sh
@@ -1,3 +1,4 @@
+clear
 export TIMEZONE="America/Sao_Paulo"
 export LANGUAGE="pt_BR"
 export UNICODE="UTF-8"
@@ -9,8 +10,8 @@ echo "docker-compose build $DOT_NET_CORE_ASPNET_VERSION"
 docker-compose -f ../docker-compose.build.yml build
 echo "------------------------------------------------------"
 
-echo
-echo "------------------------------------------------------"
-echo "docker push tjmt/dotnetcore:aspnet-$DOT_NET_CORE_ASPNET_VERSION"
-docker push tjmt/dotnetcore:aspnet-$DOT_NET_CORE_ASPNET_VERSION
-echo "------------------------------------------------------"
+# echo
+# echo "------------------------------------------------------"
+# echo "docker push tjmt/dotnetcore:aspnet-$DOT_NET_CORE_ASPNET_VERSION"
+# docker push tjmt/dotnetcore:aspnet-$DOT_NET_CORE_ASPNET_VERSION
+# echo "------------------------------------------------------"
